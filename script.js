fa7a11b542fb93a603dc0ba3ff66b6f5305e20e7
Tom Hinkle <tmhinkle@gmail.com>
2021-10-04 11:21:15 -0400

    Latest & greatest: ++code to embed script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Script loaded");
 function loadDescriptors() {
-  console.log("Loaded");
+  console.log("load descriptors");
   let nodes = document.querySelectorAll(".descriptor");
   nodes.forEach((node) => {
     let url = node.getAttribute("data-ref");
@@ -12,29 +12,50 @@ function loadDescriptors() {
   });
 }
 
-async function fetchDataForNode(node, url) {
+const PORTFOLIO_BASE = "https://thinkle-iacs.github.io/web-design-portfolio/";
+const PORTFOLIO_SOURCE =
+  "https://github.com/thinkle-iacs/web-design-portfolio/tree/master";
+/**
+ * Fetch data from web-design-portfolio github for node with URL stub.
+ * This allows us to update descriptors dynamically for student portfolios.
+ *
+ * Path is a path on our web-design-portfolio file tree in github.
+ * Node is an HTML node whose content will be replaced with the content
+ * from github.
+ * **/
+async function fetchDataForNode(node, path) {
   console.log(url);
-  let response = await fetch(
-    `https://thinkle-iacs.github.io/web-design-portfolio/${url}`
-  );
+  let response = await fetch(`${PORTFOLIO_BASE}/${path}`);
   if (response.status == 200) {
     let text = await response.text();
     node.innerHTML = text;
   } else {
-    SOURCE_URL =
-      "https://github.com/thinkle-iacs/web-design-portfolio/tree/master";
     node.innerHTML = `
-    <p style="padding: 5px;
+    <div style="padding: 5px;
               background-color:red;
               color:white;
               
     ">
-      Oops, <a href="${SOURCE_URL}/${url}">${url}</a> does not exist. Check list of skills at the
-      <a href="${SOURCE_URL}">portfolio source tree</a> 
+      Oops, <a href="${PORTFOLIO_SOURCE}/${path}">${path}</a> does not exist. Check list of skills at the
+    <a href="${PORTFOLIO_BASE}">portfolio tree</a> or browser the 
+      <a href="${PORTFOLIO_SOURCE}">source</a> 
       to see what you meant to provide instead
-    </p>`;
+    </div>`;
     console.log(
-      `You provided a data reference ${url} which does not exist. Check ${SOURCE_URL} source code to see what you meant to provide instead.`
+      `You provided a data reference ${path} which does not exist. Check ${SOURCE_URL} source code to see what you meant to provide instead.`
     );
   }
+}
+
+function displayCode() {
+  console.log("display code?");
+  let fakeScriptTags = document.querySelectorAll("code script");
+  fakeScriptTags.forEach((node) => {
+    let text = node.innerText;
+    text = text.replace(/[&]/g, "&amp;");
+    text = text.replace(/</g, "&lt;");
+    text = text.replace(/>/g, "&gt;");
+    node.outerHTML = text;
+  });
+  hljs.highlightAll();
 }
